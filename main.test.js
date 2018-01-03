faa69bbb5e7b95af2b541003a9b98e78903267b3
Ernoff <ernest.offiong@gmail.com>
2018-01-03 13:10:53 +0100

    Sinon used for stubbing

--- main.test.js
+++ main.test.js
@@ -2,23 +2,29 @@
 
 // npm packages
 const nock = require('nock');
-
+const sinon = require('sinon');
+const inquirer = require('inquirer');
 // our packages
 const {getData, handler} = require('./main');
 
 const gamesCrate = {
-    id: '578080',
-    name : 'GAMESCOM INVITATIONAL CRATE'
+  id: '578080',
+  name : 'GAMESCOM INVITATIONAL CRATE'
 };
+
+// mocks opn function
+jest.mock('opn');
+const opn = require('opn');
 
 describe('Main function', () => {
   let steamComm;
 
   beforeAll(() => {
+
     const url = `/market/priceoverview/?appid=${gamesCrate.id}&country=DE&currenccy=3&market_hash_name=${encodeURIComponent(gamesCrate.name)}`
     steamComm = nock('http://steamcommunity.com')
       .get(url)
-      .twice()
+      .times(3)
       .reply(200, {
         lowest_price: "$7.25 USD",
         median_price: "$7.19 USD",
@@ -38,7 +44,30 @@ describe('Main function', () => {
   });
 
   test('Renders correct data to a User', async done => {
+    //stub the reply from user
+    const inqStub = sinon.stub(inquirer, 'prompt').callsFake(() => ({openBrowser:false}))
+    
+    // tab in console log function
+    const consoleSpy = sinon.spy(console, 'log');
+    // execute main logic
     await handler(gamesCrate);
+    consoleSpy.restore();
+    expect(consoleSpy.args).toMatchSnapshot();
+    inqStub.restore();
     done();
   });
+
+  test('Open browser on user request', async done => {
+    //stub the reply from user
+    const inqStub = sinon.stub(inquirer, 'prompt').callsFake(() => ({openBrowser:true}));
+    const consoleSpy = sinon.spy(console, 'log');
+    await handler(gamesCrate);
+    consoleSpy.restore();
+    expect(consoleSpy.args).toMatchSnapshot();
+    expect(opn).toBeCalled();
+    inqStub.restore();
+    done();
+  });
+
+
 });
